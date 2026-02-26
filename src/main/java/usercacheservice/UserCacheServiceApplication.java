93c253ba691ebaf2d68301bab43cdbf24a4edfa7
noyrada <noy.rada.unique@gmail.com>
2026-02-26 09:54:54 +0700

    first commit

---
+++ src/main/java/usercacheservice/UserCacheServiceApplication.java
@@ -0,0 +1,13 @@
+package usercacheservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCacheServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCacheServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/usercacheservice/UserCacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package usercacheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCacheServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
